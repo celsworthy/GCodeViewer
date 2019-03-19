64d3bfba0ded6705ad88f081eb740881b43ccaf9
Tony Aldhous <tonyaldhous@cel-uk.com>
2019-03-19 11:43:02 +0000

    Add GCodeViewer obfuscation script for linux.

--- gcodeviewer_proguard.sh
+++ gcodeviewer_proguard.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 echo "Proguard: " ${USE_PROGUARD}
-if [ $1 == "true" ]; then
+mkdir -p release
+rm -rf release/*
+cp target/GCodeViewer.jar release/GCodeViewer.jar
+cp GCodeViewer.json release/GCodeViewer.json
+cp GCodeViewer.configFile.xml release/GCodeViewer.configFile.xml
+cp -r Language release/Language
+cp -r target/lib release/lib
+if [ ${USE_PROGUARD} == "true" ]; then
 	echo Run Proguard
 	mkdir -p proguard
 	rm -f proguard/*.jar
 	/home/ubuntu/proguard/bin/proguard.sh @gcodeviewer_proguard_linux.config
-	cp -f proguard/GCodeViewerObfuscated.jar target/GCodeViewer.jar
-	cp -f proguard/Configuration-1.2Obfuscated.jar target/lib/Configuration-1.2.jar
-	cp -f proguard/Stenographer-1.9Obfuscated.jar target/lib/Stenographer-1.9.jar
-	cp -f proguard/Licence-1.0Obfuscated.jar target/lib/Licence-1.0.jar
 	cp proguard.mapping target/proguard.mapping.gcodeviewer.${RELEASE_VERSION}
+	cp -f proguard/GCodeViewerObfuscated.jar release/GCodeViewer.jar
+	cp -f proguard/Configuration-1.2Obfuscated.jar release/lib/Configuration-1.2.jar
+	cp -f proguard/Stenographer-1.9Obfuscated.jar release/lib/Stenographer-1.9.jar
+	cp -f proguard/Licence-1.0Obfuscated.jar release/lib/Licence-1.0.jar
 fi
+zip -r GCodeViewer.zip release/*
