56170c0fe667979282c20a8c4d9324a92b78d140
Tony Aldhous <tonyaldhous@cel-uk.com>
2019-03-19 10:25:08 +0000

    Corrected Windows script to obfuscate.

--- gcodeviewer_proguard.bat
+++ gcodeviewer_proguard.bat
@@ -1,12 +1,20 @@
-SET RELEASE_VERSION 3.03.00
+set RELEASE_VERSION=3.03.00_A8
 if not exist "%PROGUARD_HOME%" set PROGUARD_HOME=D:\CEL\proguard6.0.3
-if not exist proguard mkdir proguard
-if exist proguard\*.jar del proguard\*.jar
+if exist release rmdir /S/Q release
+mkdir release
 
+copy /Y target\GCodeViewer.jar release\GCodeViewer.jar
+copy /Y GCodeViewer.json release\GCodeViewer.json
+copy /Y GCodeViewer.configFile.xml release\GCodeViewer.configFile.xml
+robocopy /S Language release\Language
+robocopy /S target/lib release/lib
+
+if not exist proguard mkdir proguard
+if exist proguard\*.jar del /Y proguard\*.jar
 java -jar "%PROGUARD_HOME%\lib\proguard.jar" @gcodeviewer_proguard.config
-copy /Y proguard\GCodeViewerObfuscated.jar target\GCodeViewer.jar
-copy /Y proguard\Configuration-1.2Obfuscated.jar target\lib\Configuration-1.2.jar
-copy /Y proguard\Stenographer-1.9Obfuscated.jar target\lib\Stenographer-1.9.jar
-copy /Y proguard\Licence-1.0Obfuscated.jar target\lib\Licence-1.0.jar
-copy /Y proguard\Licence-1.0Obfuscated.jar target\lib\Licence-1.0.jar
-copy /Y proguard.mapping target\proguard.mapping.gcodeviewer.%RELEASE_VERSION%
+copy /Y proguard\GCodeViewerObfuscated.jar release\GCodeViewer.jar
+copy /Y proguard\Configuration-1.2Obfuscated.jar release\lib\Configuration-1.2.jar
+copy /Y proguard\Stenographer-1.9Obfuscated.jar release\lib\Stenographer-1.9.jar
+copy /Y proguard\Licence-1.0Obfuscated.jar release\lib\Licence-1.0.jar
+copy /Y proguard.mapping proguard.mapping.gcodeviewer.%RELEASE_VERSION%
+
