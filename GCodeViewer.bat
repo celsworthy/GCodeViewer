715ea66621a605b4a37963082a4a8f83550cbf94
taldhous <tonyaldhous@cel-uk.com>
2020-04-20 16:51:45 +0100

    Update paths.

--- GCodeViewer.bat
+++ GCodeViewer.bat
@@ -5,8 +5,8 @@ if NOT DEFINED GCODE_VIEWER_JSON set GCODE_VIEWER_JSON=%GCODE_VIEWER_HOME%
 if NOT DEFINED GCODE_VIEWER_CONFIG set GCODE_VIEWER_CONFIG=%GCODE_VIEWER_HOME%\GCodeViewer.configFile.xml
 if NOT DEFINED JAVA_HOME set JAVA_HOME=%CEL_HOME%\AutoMaker\java\bin
 
-set GCODE_VIEWER_JAR=D:\CEL\Java11\GCodeViewer\target\GCodeViewer.jar
-set GCODE_VIEWER_JSON=D:\CEL\Java11\GCodeViewer
-set GCODE_VIEWER_CONFIG=D:\CEL\Java11\GCodeViewer\GCodeViewerFake.configFile.xml
+set GCODE_VIEWER_JAR=D:\CEL\GitHub\GCodeViewer\release\GCodeViewer.jar
+set GCODE_VIEWER_JSON=D:\CEL\GitHub\GCodeViewer\release
+set GCODE_VIEWER_CONFIG=D:\CEL\GitHub\GCodeViewer\GCodeViewerFake.configFile.xml
 
 "%JAVA_HOME%\bin\java.exe" -DlibertySystems.configFile="%GCODE_VIEWER_CONFIG%" -jar "%GCODE_VIEWER_JAR%" -cd "%GCODE_VIEWER_JSON%" %*
